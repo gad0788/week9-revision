e27e17c8f509a5cab3ca94a505a85642316076fb
Gad ndayishimiye <nday.gady@gmail.com>
2024-03-17 01:43:07 -0700

    alex_app

--- main.tf
+++ main.tf
@@ -4,4 +4,12 @@ resource "aws_lightsail_instance" "server1" {
   bundle_id = "medium_1_0"
   availability_zone = "us-east-1a"
   key_pair_name = "lamp"
+  user_data = file ("resume.sh")
+}
+resource "aws_route53_record" "rc1" {
+  zone_id = "Z0319926K2HLBRS8NWD0"
+  type = "A"
+  ttl = 300
+  name = "resume.kayband.com"
+  records = [ aws_lightsail_instance.server1.public_ip_address ]
 }
